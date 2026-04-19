f11558b0a79acb69e72a65c38ae932705383748c
Brandon Jahir Sanchez Rodriguez <jahirrod8@gmail.com>
2026-04-18 23:07:31 -0600

    Primer metodo

--- pruebas.py
+++ pruebas.py
@@ -1,3 +1,14 @@
 class Personaje:
     def __init__(self, nombre, vida, ataque, tipo_personaje, rol):
-        pass
+        self.nombre = nombre
+        self.vida = vida
+        self.ataque = ataque
+        self.tipo_personaje = tipo_personaje
+        self.rol = rol
+    
+    def info_personaje(self):
+        print (f"Nombre:{self.nombre} ")
+        print (f"Vida:{self.vida} ")
+        print (f"Ataque:{self.ataque} ")
+        print (f"Tipo de personaje:{self.tipo_personaje} ")
+        print (f"rol:{self.rol} ")
